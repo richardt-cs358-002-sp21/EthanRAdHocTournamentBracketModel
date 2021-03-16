bc3c5a3002b5b0cca831be0f83ff01e0a5bf3b6e
ReedEthan <ReedEthan@192.168.1.47>
2021-03-16 17:26:49 -0500

    created tests

--- src/TeamManager.java
+++ src/TeamManager.java
@@ -1,8 +1,58 @@
 
 public class TeamManager {
+  
+  private int idealTeamSize;
+  private int minTeamSize;
+  private int maxTeamSize;
 
   public TeamManager() {
-    // TODO Auto-generated constructor stub
+    
+    idealTeamSize = 2;
+    minTeamSize = 2;
+    maxTeamSize = 2;
   }
+    /**
+     * Method for setting three integer variables
+     * @param ideal Perferable team size
+     * @param min Minimum team size
+     * @param max Maximum team size
+     */
+    
+    public void setSize(int ideal, int min, int max) {
+      idealTeamSize = ideal;
+      minTeamSize = min;
+      maxTeamSize = max;
+    }
+    /**
+     * Get ideal team size
+     * 
+     * @return idealTeamSize
+     */
+    public int getIdealTeamSize() {
 
-}
+      return idealTeamSize;
+
+    }
+
+    /**
+     * Get minimum team size
+     * 
+     * @return minTeamSize
+     */
+    public int getMinTeamSize() {
+
+      return minTeamSize;
+
+    }
+
+    /**
+     * Get maximum team size
+     * 
+     * @return maxTeamSize
+     */
+    public int getMaxTeamSize() {
+
+      return maxTeamSize;
+
+    }
+  }
